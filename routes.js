9f16fcb21a70d5254637875e2e3f4d270ec77654
Eric Katz <ericpkatz@gmail.com>
2017-02-02 19:57:48 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -13,10 +13,12 @@ router.delete('/:id', function(req, res, next){
   res.redirect('/');
 });
 
+//this should be a get for seeing the edit form
 router.patch('/:id', function(req, res, next){
   res.render('productEdit', {title: 'Edit Product', id: req.params.id * 1, product: products.getProduct(req.params.id * 1)});
 });
 
+//this should be a patch to /products/:id
 router.post('/edit-product/:id', function(req, res, next){
   var product = req.body.name;
   var id = req.params.id * 1;
@@ -24,6 +26,7 @@ router.post('/edit-product/:id', function(req, res, next){
   res.redirect('/');
 });
 
+//this should just be a post to /products
 router.post('/new_product', function(req, res) {
   var product = req.body.product;
   products.addProduct(product);
